278550cb3fbda0bed93f7f70f2298f1d3d6e9af1
Ruhul <ruhulthisis@gmail.com>
2025-03-08 14:36:46 +0600

    database connected

--- service/mongo.js
+++ service/mongo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function dbConnect(){
     try {
         const conn = await mongoose.connect(String(process.env.MONGODB_CONNECTION_URI));
-        return conn;
+        return conn
     } catch(err) {
         console.error(err);
     }
