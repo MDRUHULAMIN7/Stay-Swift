507b08cb67297cea33ced4901315dc813159e351
Ruhul <ruhulthisis@gmail.com>
2025-03-09 11:53:01 +0600

    login complete

--- auth.js
+++ auth.js
@@ -1,5 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import mongoClientPromise from "./database/mongoClientPromise";
+import { dbConnect } from "./service/mongo";
+import { UserModel } from "./models/user-model";
 
 export const {
     handlers:{GET,POST},
@@ -7,11 +12,42 @@ export const {
     signIn,
     signOut,
    }=NextAuth({
-
+  adapter:MongoDBAdapter(mongoClientPromise,{
+    databaseName: process.env.ENVIRONMENT}),
+    session:{
+        strategy: "jwt",
+    },
     providers:[
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        CredentialsProvider({
+            credentials:{
+                email: {},
+                password: {},
+            },
+            async authorize(credentials){
+                if(credentials === null){
+                    return null;
+                }
+                try{
+               const user = await UserModel.findOne({email:credentials.email});
+               if(user){
+                   const isMatch = user.email === credentials.email;
+                   if(isMatch){
+                       return user;
+                   }else{
+                       throw new Error("Email or Password is incorrect");}
+               }
+               else{
+                   throw new Error("User not found");
+               }
+                }catch(e){
+                    throw new Error(e);
+                }
+            }
+                
         })
     ]
 })
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,56 +1,65 @@
 import Link from "next/link";
 import Image from "next/image";
+import { auth } from "@/auth";
+import Logout from "./auth/Logout";
 
-// import Logout from "./auth/Logout";
-// import { auth } from "@/service/auth";
 const Navbar = async ({ sideMenu }) => {
-  // const session = await auth();
-  // console.log(session);
-  return (
-    <nav>
-      <Link href="/">
-        <Image
-          src="/nav-logo.png"
-          alt="Stay Swift Logo"
-          width={200}
-          height={200}
-        />
-      </Link>
-      {sideMenu && (
-        <ul>
-          <li>
-            <Link href="#">Recommended Places</Link>
-          </li>
+    const session = await auth();
+    console.log(session);
+    return (
+        <nav>
+            <Link href="/">
+                <Image
+                    src="/nav-logo.png"
+                    alt="Stay Swift Logo"
+                    width={200}
+                    height={200}
+                />
+            </Link>
+            {sideMenu && (
+                <ul>
+                    <li>
+                        <Link href="#">Recommended Places</Link>
+                    </li>
 
-          <li>
-            <Link href="#">About Us</Link>
-          </li>
+                  
 
-          <li>
-            <Link href="#">Contact us</Link>
-          </li>
+                    <li>
+                        <Link href="#">Contact us</Link>
+                    </li>
 
-          <li>
-            <Link href="/bookings">Bookings</Link>
-          </li>
+                    <li>
+                        <Link href="/bookings">Bookings</Link>
+                    </li>
 
-          <li>
-            {sideMenu ? (
-              <idiv>
-                {/* <span className="mx-1"> {session?.user?.name} </span> */}
-                <span> | </span>
-                {/* <Logout /> */}
-              </idiv>
-            ) : (
-              <Link href="/login" className="login">
-                Login
-              </Link>
+                    <li>
+                        {
+                            session?.user ? (
+                                <idiv className="flex items-center gap-2">
+                                    <span className="flex justify-center items-center">
+                                    <span className="mx-1"> {session?.user?.name} </span>
+                                    
+                                    <Image
+                                        src={session?.user?.image}
+                                        alt={session?.user?.name}
+                                        className="rounded-full h-10 w-10"
+                                        width={32}
+                                        height={22}/>
+                                    </span>
+                                  
+                                    <span> | </span>
+                                    <Logout />
+                                </idiv>
+                            ) : ( <Link href="/login" className="login">
+                            Login
+                        </Link>)
+                        }
+
+                    </li>
+                </ul>
             )}
-          </li>
-        </ul>
-      )}
-    </nav>
-  );
+        </nav>
+    );
 };
 
 export default Navbar;
