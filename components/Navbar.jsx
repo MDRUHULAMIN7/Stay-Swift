0fd131c5d2521fc9010369527d7c37bf1e83f3e4
Ruhul <ruhulthisis@gmail.com>
2025-03-09 09:35:27 +0600

    google login

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,41 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
 
-import Link from "next/link"
-import Image from "next/image"
-
-const Navbar = () => {
+// import Logout from "./auth/Logout";
+// import { auth } from "@/service/auth";
+const Navbar = async ({ sideMenu }) => {
+  // const session = await auth();
+  // console.log(session);
   return (
     <nav>
-    <Link href="/">
-      <Image 
-        src="/nav-logo.png" 
-        alt="Stay Swift Logo" 
-        width={200}
-        height={300} />
-    </Link>
+      <Link href="/">
+        <Image
+          src="/nav-logo.png"
+          alt="Stay Swift Logo"
+          width={200}
+          height={200}
+        />
+      </Link>
+      {sideMenu && (
+        <ul>
+          <li>
+            <Link href="#">Recommended Places</Link>
+          </li>
 
-    <ul>
-      <li>
-        <Link href="#">Recommended Places</Link>
-      </li>
+          <li>
+            <Link href="#">About Us</Link>
+          </li>
 
-      <li>
-        <Link href="#">About Us</Link>
-      </li>
+          <li>
+            <Link href="#">Contact us</Link>
+          </li>
 
-      <li>
-        <Link href="#">Contact us</Link>
-      </li>
+          <li>
+            <Link href="/bookings">Bookings</Link>
+          </li>
 
-      <li>
-        <Link href="/bookings">Bookings</Link>
-      </li>
+          <li>
+            {sideMenu ? (
+              <idiv>
+                {/* <span className="mx-1"> {session?.user?.name} </span> */}
+                <span> | </span>
+                {/* <Logout /> */}
+              </idiv>
+            ) : (
+              <Link href="/login" className="login">
+                Login
+              </Link>
+            )}
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
 
-      <li>
-        <Link href="/login" >Login</Link>
-      </li>
-    </ul>
-  </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
